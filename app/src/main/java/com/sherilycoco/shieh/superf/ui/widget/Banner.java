a9c61f62a96bea6c9434465f4c8fc41193cdd642
xiejiali <xiejiali@homepaas.com>
2016-08-31 18:11:57 +0800

    Popuwindow  recyclerview中的数据无法显示

---
+++ app/src/main/java/com/sherilycoco/shieh/superf/ui/widget/Banner.java
@@ -0,0 +1,23 @@
+package com.sherilycoco.shieh.superf.ui.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by Administrator on 2016/8/25.
+ */
+public class Banner extends FrameLayout {
+    public Banner(Context context) {
+        super(context);
+    }
+
+    public Banner(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Banner(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
