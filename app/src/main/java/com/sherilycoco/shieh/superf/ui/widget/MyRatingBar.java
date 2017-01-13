a6c8d72f25cb399f54dcfbbfdcad22eed29c95f1
xiejiali <xiejiali@homepaas.com>
2017-01-13 14:00:01 +0800

    五星评分

---
+++ app/src/main/java/com/sherilycoco/shieh/superf/ui/widget/MyRatingBar.java
@@ -0,0 +1,24 @@
+package com.sherilycoco.shieh.superf.ui.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Administrator on 2016/12/26.
+ */
+
+public class MyRatingBar extends LinearLayout {
+
+    public MyRatingBar(Context context) {
+        super(context);
+    }
+
+    public MyRatingBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyRatingBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
